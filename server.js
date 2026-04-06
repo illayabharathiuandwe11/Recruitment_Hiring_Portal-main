2800c70fb0362b896e9a88a9e8d458a533bec31d
illayabharathiuandwe11 <illayabharathiuandwe11@gmail.com>
2026-04-06 13:01:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://myuandwe.netlify.app/"
+  "https://myuandwe.netlify.app"
 ];
 
 app.use((req, res, next) => {
